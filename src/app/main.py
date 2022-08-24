29c6513965f20349f2c0406c6cff75fc55340261
minio999 <minkowskidominik03@gmail.com>
2022-08-24 11:53:25 +0200

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,120 +1,213 @@
 '''
-main module for app
+main module
 '''
-
 
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
 
-RANDOM_STATE = 42
-MIN_SAMPLE_SPLIT=4
-MIN_SAMPLES_LEAF=5
-N_ESTIMATORS=100
+TRAIN_PATH = './data/train.csv'
+TEST_PATH = './data/test.csv'
 N_SPLITS = 5
-USELESS_FEATURES = ["PassengerId", "Name", "Ticket", "Cabin",
-                    "Parch", "SibSp", 'Embarked', 'Fare']
-PATH = './data/train.csv'
-df_raw = pd.read_csv(PATH)
+RANDOM_STATE = 42
 
 
-def impute_age(df, value):
+def extracting_title_age_imputing(data_df, train_df, test_df):
     '''
-    Replaces Nulls in column "Age" of a dataframe with the passed value
+    Function to extract title and impute age
 
     Parameters:
         dataframe (pandas.DataFrame): DataFrame on which to operate
-        value (float): Value used for imputation
-    Returns:
-        pandas.DataFrame
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
     '''
+    data_df['Title'] = data_df['Name']
 
-    df['Age'] = df["Age"].fillna(value)
-    return df
+    for name_string in data_df['Name']:
+        data_df['Title'] = data_df['Name'].str.extract('([A-Za-z]+)\.', expand=True)
 
+    mapping = {'Mlle': 'Miss', 'Major': 'Mr', 'Col':
+                'Mr', 'Sir': 'Mr', 'Don': 'Mr', 'Mme': 'Miss',
+                'Jonkheer': 'Mr', 'Lady': 'Mrs', 'Capt':
+                'Mr', 'Countess': 'Mrs', 'Ms': 'Miss', 'Dona': 'Mrs'}
+    data_df.replace({'Title': mapping}, inplace=True)
 
-def convert_sex(df):
+    titles = ['Dr', 'Master', 'Miss', 'Mr', 'Mrs', 'Rev']
+    for title in titles:
+        age_to_impute = data_df.groupby('Title')['Age'].median()[titles.index(title)]
+        data_df.loc[(data_df['Age'].isnull()) & (data_df['Title'] == title), 'Age'] = age_to_impute
+
+    train_df['Age'] = data_df['Age'][:891]
+    test_df['Age'] = data_df['Age'][891:]
+    data_df.drop('Title', axis = 1, inplace = True)
+
+def family_size(data_df, train_df, test_df):
     '''
-    Replacing sex in column "Sex" of a dataframe to 1 if it's male and 0 if it's female
+    Function to combine Parch and Sibsp columns to get family size on board
 
     Parameters:
-        dataframe (pandas.DataFrame): Dataframe on which to operate
-    Returns:
-        pandas.DataFrame
+        dataframe (pandas.DataFrame): DataFrame on which to operate
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
     '''
+    data_df['Family_Size'] = data_df['Parch'] + data_df['SibSp']
+    train_df['Family_Size'] = data_df['Family_Size'][:891]
+    test_df['Family_Size'] = data_df['Family_Size'][891:]
 
-    df['is_male'] = 0
-    df.loc[df['Sex'] == 'male', 'is_male'] = 1
-    df = df.drop(columns=['Sex'])
-    return df
-
-def count_relatives_on_board(df):
+def spliting_name(data_df):
     '''
-    Counting Relatives on board based of sibsp and parch columns
+    Function to split name column to name and last name
 
-    Paramters:
-        dataframe (pandas.DataFrame): Dataframe on which to operate
-    Retruns:
-        pandas.DataFrame
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
     '''
-
-    df["RelativesOnboard"] = df["SibSp"] + df["Parch"]
-    return df
-
-def set_title(df):
+    data_df['Last_Name'] = data_df['Name'].apply(lambda x: str.split(x, ",")[0])
+    
+def imputing_fare(data_df):
     '''
-    Changing name titles to cryptonims
+    Function to impute fare column
 
-    Paramters:
-        dataframe (pandas.DataFrame): Dataframe on which to operate
-    Retruns:
-        pandas.DataFrame
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
     '''
+    data_df['Fare'].fillna(data_df['Fare'].mean(), inplace=True)
 
-    mapping = {'Mlle': 'Miss', 'Major': 'Mr', 'Col': 'Mr', 'Sir': 'Mr', 'Don': 'Mr', 'Mme': 'Miss',
-      'Jonkheer': 'Mr', 'Lady': 'Mrs', 'Capt': 'Mr', 'Countess': 'Mrs', 'Ms': 'Miss', 'Dona': 'Mrs'}
 
-    df['Title'] = df['Name'].str.extract('([A-Za-z]+)\.', expand=True)
-    df.replace({'Title': mapping}, inplace=True)
-    return df
-
-def title_encode(df):
-    label_encoder = LabelEncoder()
-    df['Title'] = label_encoder.fit_transform(df['Title'])
-    return df
-
-def transform_data(df, mean_age_value):
+def family_survival(data_df, train_df, test_df):
     '''
-    Applying data cleaning functions to data sets
+    Function to get family survival rate
 
-    Paramters:
-        dataframe (pandas.DataFrame): Dataframe on which to operate
-        mean_age (float): Mean age of training data set
-    Retruns:
-        pandas.DataFrame
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
     '''
-    df = set_title(df)
-    df = count_relatives_on_board(df)
-    df = impute_age(df, mean_age_value)
-    df = convert_sex(df)
-    df = title_encode(df)
-    df.drop(USELESS_FEATURES, inplace=True,axis=1)
-    return df
+    DEFAULT_SURVIVAL_VALUE = 0.5
+    data_df['Family_Survival'] = DEFAULT_SURVIVAL_VALUE
+
+    for grp, grp_df in data_df[['Survived','Name', 'Last_Name', 'Fare', 'Ticket', 'PassengerId',
+                            'SibSp', 'Parch', 'Age', 'Cabin']].groupby(['Last_Name', 'Fare']):
+
+        if (len(grp_df) != 1):
+            # A Family group is found.
+            for ind, row in grp_df.iterrows():
+                smax = grp_df.drop(ind)['Survived'].max()
+                smin = grp_df.drop(ind)['Survived'].min()
+                passID = row['PassengerId']
+                if (smax == 1.0):
+                    data_df.loc[data_df['PassengerId'] == passID, 'Family_Survival'] = 1
+                elif (smin==0.0):
+                    data_df.loc[data_df['PassengerId'] == passID, 'Family_Survival'] = 0
+    for _, grp_df in data_df.groupby('Ticket'):
+        if (len(grp_df) != 1):
+            for ind, row in grp_df.iterrows():
+                if (row['Family_Survival'] == 0) | (row['Family_Survival']== 0.5):
+                    smax = grp_df.drop(ind)['Survived'].max()
+                    smin = grp_df.drop(ind)['Survived'].min()
+                    passID = row['PassengerId']
+                    if (smax == 1.0):
+                        data_df.loc[data_df['PassengerId'] == passID, 'Family_Survival'] = 1
+                    elif (smin==0.0):
+                        data_df.loc[data_df['PassengerId'] == passID, 'Family_Survival'] = 0
+
+    # # Family_Survival in TRAIN_DF and TEST_DF:
+    train_df['Family_Survival'] = data_df['Family_Survival'][:891]
+    test_df['Family_Survival'] = data_df['Family_Survival'][891:]
+
+def fare_bin(data_df, train_df, test_df):
+    '''
+    Function to get fare bin and encode it
+
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
+    '''
+    data_df['Fare'].fillna(data_df['Fare'].median(), inplace = True)
+    data_df['FareBin'] = pd.qcut(data_df['Fare'], 5)
+
+    label = LabelEncoder()
+    data_df['FareBin_Code'] = label.fit_transform(data_df['FareBin'])
+
+    train_df['FareBin_Code'] = data_df['FareBin_Code'][:891]
+    test_df['FareBin_Code'] = data_df['FareBin_Code'][891:]
+
+    train_df.drop(['Fare'], axis=1, inplace=True)
+    test_df.drop(['Fare'], axis=1, inplace=True)
+
+def age_bin(data_df, train_df, test_df):
+    '''
+    Function to get age bin and encode it
+
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
+    '''
+    data_df['AgeBin'] = pd.qcut(data_df['Age'], 4)
+
+    label = LabelEncoder()
+    data_df['AgeBin_Code'] = label.fit_transform(data_df['AgeBin'])
+
+    train_df['AgeBin_Code'] = data_df['AgeBin_Code'][:891]
+    test_df['AgeBin_Code'] = data_df['AgeBin_Code'][891:]
+
+    train_df.drop(['Age'], axis=1, inplace=True)
+    test_df.drop(['Age'], axis=1, inplace=True)
+
+def replacing_sex(train_df, test_df):
+    '''    
+    Function to replace sex to male or female
+
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
+    '''
+    train_df['Sex'].replace(['male','female'],[0,1],inplace=True)
+    test_df['Sex'].replace(['male','female'],[0,1],inplace=True)
+
+def aplying_preprocesing(data_df,train_df, test_df):
+    '''
+    Function to apply preprocesing and drop columns
+
+    Parameters:
+        dataframe (pandas.DataFrame): DataFrame on which to operate
+        dataframe (pandas.DataFrame): DataFrame on which to train
+        dataframe (pandas.DataFrame): DataFrame on which to test
+    '''
+    extracting_title_age_imputing(data_df, train_df, test_df)
+    family_size(data_df, train_df, test_df)
+    spliting_name(data_df)
+    imputing_fare(data_df)
+    family_survival(data_df, train_df, test_df)
+    fare_bin(data_df, train_df, test_df)
+    age_bin(data_df, train_df, test_df)
+    replacing_sex(train_df, test_df)
+    train_df.drop(['Name', 'PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin',
+               'Embarked'], axis = 1, inplace = True)
+    test_df.drop(['Name','PassengerId', 'SibSp', 'Parch', 'Ticket', 'Cabin',
+              'Embarked'], axis = 1, inplace = True)
 
 def main():
     '''
-    Main Function
+    Main function
     '''
+    train_df = pd.read_csv(TRAIN_PATH)
+    test_df = pd.read_csv(TEST_PATH)
+    data_df = pd.concat([train_df, test_df])
 
-    LABEL = 'Survived'
-    mean_age = df_raw['Age'].mean()
-    df = transform_data(df_raw, mean_age)
-    X = df
-    X = X.drop('Survived', axis=1)
-    y = df['Survived']
+    aplying_preprocesing(data_df, train_df, test_df)
+
+    X = train_df.drop('Survived', axis=1)
+    y = train_df['Survived']
+    X_test_kaggle = test_df.copy()
+
+    std_scaler = StandardScaler()
+    X = std_scaler.fit_transform(X)
+    X_test = std_scaler.transform(X_test)
 
     k_fold = KFold(
         n_splits=N_SPLITS,
@@ -125,15 +218,14 @@ def main():
     scores = []
 
     for train_index, test_index in k_fold.split(X):
-        X_train, X_test = X.loc[train_index], X.loc[test_index]
+        X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y.loc[train_index], y.loc[test_index]
 
-        clf = RandomForestClassifier(n_estimators=N_ESTIMATORS, bootstrap=True, criterion='entropy',
-                                    min_samples_leaf=MIN_SAMPLES_LEAF,
-                                    min_samples_split=MIN_SAMPLE_SPLIT, random_state=RANDOM_STATE)
-
-        clf.fit(X_train, y_train)
-        y_predict = clf.predict(X_test)
+        knn = KNeighborsClassifier(algorithm='auto', leaf_size=26, metric='minkowski',
+                           metric_params=None, n_jobs=1, n_neighbors=6, p=2,
+                           weights='uniform')
+        knn.fit(X_train, y_train)
+        y_predict = knn.predict(X_test)
 
         acc_score = round(accuracy_score(y_test, y_predict),3)
 
@@ -145,5 +237,6 @@ def main():
     print("Average:", round(100*np.mean(scores), 1), "%")
     print("Std:", round(100*np.std(scores), 1), "%")
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
